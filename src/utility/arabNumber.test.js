9cd7734850bff9d75a6996170d60cc38ff286432
PaulBlackMotion <71767507+paulcotogno@users.noreply.github.com>
2023-11-09 14:33:58 +0100

    change pipeline | add tests | change documentation

--- src/utility/arabNumber.test.js
+++ src/utility/arabNumber.test.js
@@ -1,11 +1,21 @@
 import { convertToNumber } from "./arabNumber";
 
-it('test Error', () => {
+it('Test number', () => {
+    expect(convertToNumber(89)).toEqual({error: true, message: 'Not a valid roman number'})
+    expect(convertToNumber(0)).toEqual({error: true, message: 'Not a valid roman number'})
+})
+
+it('Test string but non-roman number', () => {
+    expect(convertToNumber('hello')).toEqual({error: true, message: 'Not a valid roman number'})
+    expect(convertToNumber('hi')).toEqual({error: true, message: 'Not a valid roman number'})
+})
+
+it('Test wrong roman number', () => {
     expect(convertToNumber('IIX')).toEqual({error: true, message: 'Not a valid roman number'})
     expect(convertToNumber('CCCC')).toEqual({error: true, message: 'Not a valid roman number'})
 })
 
-it('test some roman number', () => {
+it('Test different roman number', () => {
     expect(convertToNumber('XIX')).toEqual({data: 19})
     expect(convertToNumber('MMXXIII')).toEqual({data: 2023})
 })
--- src/utility/romanNumber.test.js
+++ src/utility/romanNumber.test.js
@@ -1,16 +1,16 @@
 import { convertToRoman } from './romanNumber'
 
-it('test error number', () => {
-    expect(convertToRoman(0)).toEqual({error: true, message: 'Number too low [Valid Number "1 - 10000"]'})
-    expect(convertToRoman(32000)).toEqual({error: true, message: 'Number too high [Valid Number "1 - 10000"]'})
-})
-
-it('test not a number', () => {
+it('Test not a number', () => {
     expect(convertToRoman('string')).toEqual({error: true, message: 'Not a number'})
     expect(convertToRoman()).toEqual({error: true, message: 'Not a number'})
 })
 
-it('test random number', () => {
+it('Test non valid number', () => {
+    expect(convertToRoman(0)).toEqual({error: true, message: 'Number too low [Valid Number "1 - 10000"]'})
+    expect(convertToRoman(32000)).toEqual({error: true, message: 'Number too high [Valid Number "1 - 10000"]'})
+})
+
+it('Test some valid number', () => {
     expect(convertToRoman(7)).toEqual({data: 'VII'})
     expect(convertToRoman(338)).toEqual({data: 'CCCXXXVIII'})
     expect(convertToRoman(6923)).toEqual({data: 'MMMMMMCMXXIII'})
